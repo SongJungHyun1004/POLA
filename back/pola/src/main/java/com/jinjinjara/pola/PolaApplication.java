e7485b07724e41a3204ce622ff6bfcbade80ecaa
정유진 <dw8624@naver.com>
2025-10-24 17:36:44 +0900

    Feat: 백엔드 폴더 구조 생성 완료

---
+++ back/pola/src/main/java/com/jinjinjara/pola/PolaApplication.java
@@ -0,0 +1,13 @@
+package com.jinjinjara.pola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolaApplication.class, args);
+	}
+
+}
