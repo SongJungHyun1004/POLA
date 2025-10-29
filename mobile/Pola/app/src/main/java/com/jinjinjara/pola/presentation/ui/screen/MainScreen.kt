950658dd8de01595f8a4b65eed90395b216cb27a
송정현 <vic656@naver.com>
2025-10-29 10:17:18 +0900

    Feat: Bottom navigation bar 및 아이콘 크기 조절

--- mobile/Pola/app/src/main/java/com/jinjinjara/pola/presentation/ui/screen/MainScreen.kt
+++ mobile/Pola/app/src/main/java/com/jinjinjara/pola/presentation/ui/screen/MainScreen.kt
@@ -1,7 +1,11 @@
 package com.jinjinjara.pola.presentation.ui.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +15,7 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -48,6 +53,7 @@ fun MainScreen(
                     color = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.2f)
                 )
                 NavigationBar(
+                    modifier = Modifier.height(64.dp),
                     containerColor = MaterialTheme.colorScheme.background
                 ) {
                     BottomNavItem.items.forEach { item ->
@@ -57,13 +63,19 @@ fun MainScreen(
 
                         NavigationBarItem(
                             icon = {
-                                Icon(
-                                    painter = painterResource(
-                                        id = if (selected) item.selectedIcon else item.icon
-                                    ),
-                                    contentDescription = item.title,
-                                    tint = Color.Unspecified
-                                )
+                                Box(
+                                    modifier = Modifier.size(24.dp),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Icon(
+                                        painter = painterResource(
+                                            id = if (selected) item.selectedIcon else item.icon
+                                        ),
+                                        contentDescription = item.title,
+                                        tint = Color.Unspecified,
+                                        modifier = Modifier.fillMaxSize()
+                                    )
+                                }
                             },
                             selected = selected,
                             onClick = {
