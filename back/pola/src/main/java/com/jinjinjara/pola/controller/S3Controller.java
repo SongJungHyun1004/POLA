494b5ff35673491b5619669fc9f6d4bb0d20d743
이도형 <eehgud99@gmail.com>
2025-10-30 17:13:17 +0900

    Feat: Presigned URL 컨트롤러 서비스 기본틀 제작

---
+++ back/pola/src/main/java/com/jinjinjara/pola/controller/S3Controller.java
@@ -0,0 +1,31 @@
+package com.jinjinjara.pola.s3;
+
+
+import com.jinjinjara.pola.s3.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URL;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/files") // API의 기본 경로를 /api/files로 설정
+public class S3Controller {
+
+    private final S3Service s3Service;
+
+    @GetMapping("/s3/presigned/upload")
+    public Map<String, String> getUploadUrl(@RequestParam String fileName) {
+        URL presignedUrl = s3Service.generateUploadUrl(fileName);
+        return Map.of("url", presignedUrl.toString());
+    }
+
+    @GetMapping("/s3/presigned/download")
+    public Map<String, String> getDownloadUrl(@RequestParam String fileName) {
+        URL presignedUrl = s3Service.generateDownloadUrl(fileName);
+        return Map.of("url", presignedUrl.toString());
+    }
+}
---
+++ back/pola/src/main/java/com/jinjinjara/pola/s3/S3PresignedUrlResponse.java
@@ -0,0 +1,4 @@
+package com.jinjinjara.pola.s3;
+
+public class S3PresignedUrlResponse {
+}
