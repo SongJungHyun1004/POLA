f6709a276e2224c15556ec4a53b1179d9669a031
이도형 <eehgud99@gmail.com>
2025-11-03 09:46:46 +0900

    FIx: 컨트롤러 응답수정

--- back/pola/src/main/java/com/jinjinjara/pola/controller/S3Controller.java
+++ back/pola/src/main/java/com/jinjinjara/pola/controller/S3Controller.java
@@ -2,6 +2,7 @@ package com.jinjinjara.pola.controller;
 
 
 import com.jinjinjara.pola.s3.S3Service;
+import com.jinjinjara.pola.s3.dto.response.S3PresignedUrlResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,20 +11,19 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/files") // API의 기본 경로를 /api/files로 설정
+@RequestMapping("/api/v1/files") // API의 기본 경로를 /api/files로 설정
 public class S3Controller {
 
     private final S3Service s3Service;
 
     @GetMapping("/s3/presigned/upload")
-    public Map<String, String> getUploadUrl(@RequestParam String fileName) {
-        URL presignedUrl = s3Service.generateUploadUrl(fileName).getUrl();
-        return Map.of("url", presignedUrl.toString());
+    public S3PresignedUrlResponse getUploadUrl(@RequestParam String fileName) {
+        return s3Service.generateUploadUrl(fileName);
     }
 
     @GetMapping("/s3/presigned/download")
-    public Map<String, String> getDownloadUrl(@RequestParam String fileName) {
-        URL presignedUrl = s3Service.generateDownloadUrl(fileName);
-        return Map.of("url", presignedUrl.toString());
+    public S3PresignedUrlResponse getDownloadUrl(@RequestParam String key) {
+        URL presignedUrl = s3Service.generateDownloadUrl(key);
+        return new S3PresignedUrlResponse(presignedUrl, key);
     }
 }
