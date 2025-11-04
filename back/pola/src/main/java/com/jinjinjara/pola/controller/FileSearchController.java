0863c2885ffbf6619ecf27feb5687b03d6e66764
이도형 <eehgud99@gmail.com>
2025-11-04 12:55:57 +0900

    no message

--- back/pola/src/main/java/com/jinjinjara/pola/controller/FileSearchController.java
+++ back/pola/src/main/java/com/jinjinjara/pola/controller/FileSearchController.java
@@ -4,19 +4,18 @@ import com.jinjinjara.pola.common.ApiResponse;
 import com.jinjinjara.pola.search.document.FileDocument;
 import com.jinjinjara.pola.search.service.FileSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/files")
+@RequestMapping("/api/v1/files/search")
 @RequiredArgsConstructor
 public class FileSearchController {
 
     private final FileSearchService fileSearchService;
 
-    @GetMapping("/search")
+    @GetMapping
     public ApiResponse<List<FileDocument>> searchFiles(@RequestParam("q") String keyword) {
         try {
             List<FileDocument> results = fileSearchService.search(keyword);
