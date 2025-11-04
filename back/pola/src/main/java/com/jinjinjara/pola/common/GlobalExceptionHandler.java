add47d91322a9a014b1dacd4616d1d4ec6a0ef43
이도형 <eehgud99@gmail.com>
2025-11-04 14:52:02 +0900

    Feat: API 설명 추가 및 예외처리 코드 추가

--- back/pola/src/main/java/com/jinjinjara/pola/common/GlobalExceptionHandler.java
+++ back/pola/src/main/java/com/jinjinjara/pola/common/GlobalExceptionHandler.java
@@ -1,8 +1,31 @@
 package com.jinjinjara.pola.common;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
+
+    /**
+     * 커스텀 예외 처리
+     */
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        ApiResponse<Void> response = ApiResponse.fail(errorCode.getCode(), errorCode.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    /**
+     * 일반 예외 처리 (예상치 못한 오류)
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception e) {
+        ApiResponse<Void> response = ApiResponse.fail(
+                ErrorCode.INTERNAL_SERVER_ERROR.getCode(),
+                e.getMessage()
+        );
+        return new ResponseEntity<>(response, ErrorCode.INTERNAL_SERVER_ERROR.getStatus());
+    }
 }
