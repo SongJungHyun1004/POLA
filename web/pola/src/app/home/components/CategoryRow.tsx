dc04d6deb9ce50b2418177c71a501ae56cd2fd68
kim0904min <kim0904min@gmail.com>
2025-11-06 14:55:27 +0900

    Fix: SSR -> CSR에 따른 hydration 에러 수정

--- web/pola/src/app/home/components/CategoryRow.tsx
+++ web/pola/src/app/home/components/CategoryRow.tsx
@@ -1,23 +1,22 @@
+import { useEffect, useState } from "react";
 import PolaroidCard from "./PolaroidCard";
 
 export default function CategoryRow({ imgSrc }: { imgSrc: string }) {
-  const rotations = Array.from({ length: 6 }, () => {
-    const deg = Math.random() * 12 - 6;
-    return `rotate(${deg}deg)`;
-  });
+  const [rotations, setRotations] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newRotations = Array.from({ length: 5 }, () => {
+      const deg = Math.random() * 12 - 6;
+      return `rotate(${deg}deg)`;
+    });
+    setRotations(newRotations);
+  }, []);
+
+  if (rotations.length === 0) return null;
 
   return (
     <div className="flex flex-col mb-4 w-full overflow-visible">
-      <div
-        className="
-          grid 
-          grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-5 
-          gap-3
-          px-10
-          overflow-visible
-          justify-items-center
-        "
-      >
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-5 gap-3 px-10 overflow-visible justify-items-center">
         {[...Array(5)].map((_, i) => (
           <div
             key={i}
