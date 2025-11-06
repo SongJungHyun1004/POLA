5d997a261921c18f63c02a0f8107d53b1744ab2b
kim0904min <kim0904min@gmail.com>
2025-11-06 15:06:48 +0900

    Feat: home에서 User정보 획득 후 store에 저장

--- web/pola/src/app/home/page.tsx
+++ web/pola/src/app/home/page.tsx
@@ -1,10 +1,58 @@
+"use client";
+
 import CategoryBox from "./components/CategoryBox";
 import Timeline from "./components/Timeline";
 import CategoryRow from "./components/CategoryRow";
 import TextLink from "./components/TextLink";
 import Link from "next/link";
+import useAuthStore from "@/store/useAuthStore";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const router = useRouter();
+  const { setUser } = useAuthStore();
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("accessToken");
+    setAccessToken(storedToken);
+
+    if (!storedToken) {
+      router.push("/");
+      return;
+    }
+
+    const fetchUserData = async () => {
+      const res = await fetch(
+        process.env.NEXT_PUBLIC_POLA_API_BASE_URL + "/user/me",
+        {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        }
+      );
+
+      if (res.ok) {
+        const data = await res.json();
+        const { display_name, profile_image_url } = data.data;
+
+        setUser({
+          id: data.data.id,
+          email: data.data.email,
+          display_name,
+          profile_image_url,
+        });
+      } else {
+        router.push("/");
+      }
+    };
+
+    if (storedToken) {
+      fetchUserData();
+    }
+  }, [accessToken, router, setUser]);
+
   return (
     <main className="h-full text-[#4C3D25] px-8 pb-12 bg-[#FFFEF8]">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 h-full">
